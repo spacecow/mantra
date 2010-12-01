780967ec33f7ac7a1b2addc99253d1e9ac1f590e
Johan Sveholm <jsveholm@gmail.com>
2010-12-01 22:25:40 +0900

    make the #mainpage prettier

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -60,6 +60,7 @@ SimpleForm.setup do |config|
 
   # Default size for text inputs.
   config.default_input_size = 31
+  
 
   # When false, do not use translations for labels, hints or placeholders.
   # config.translate = true
