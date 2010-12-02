d7373be9a96e316e13fd71fa08af443cbb069d7c
Johan Sveholm <jsveholm@gmail.com>
2010-12-02 20:55:05 +0900

    provided open_id information

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  provider :open_id, OpenID::Store::Filesystem.new('')
+  provider :open_id, OpenID::Store::Filesystem.new('/temp')
 end
