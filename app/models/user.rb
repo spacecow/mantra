b708b6d56f6970eb6abb5e74c473161557431231
Johan Sveholm <jsveholm@gmail.com>
2010-12-02 20:31:55 +0900

    provided open_id information

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User
   attr_accessible :email, :password, :password_confirmation
 
   def apply_omniauth(omniauth)
+    self.email = omniauth['user_info']['email'] if email.blank?
     authentications.build(:provider=>omniauth['provider'], :uid=>omniauth['uid'])
   end
 
