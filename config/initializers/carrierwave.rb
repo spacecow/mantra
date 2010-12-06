789ebd5dc1aa91ff43549c106222b001baf6f7e9
Johan Sveholm <jsveholm@gmail.com>
2010-12-06 14:23:37 +0900

    modified carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  config.s3_access_key_id = 'AKIAJIZDLREAZ4NTA23Q'
-  config.s3_secret_access_key = '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'
+  config.s3_access_key_id = ENV['AMAZON_ACCESS_KEY_ID']
+  config.s3_secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
   config.s3_bucket = 'mantraupload'
 end
