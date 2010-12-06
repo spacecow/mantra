4c8e1dda5cd98953e1ad01668f040d2fed78496d
Johan Sveholm <jsveholm@gmail.com>
2010-12-06 16:09:57 +0900

    changed chach_dir for carrierwave in order to make ti work with heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,10 +12,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+#  def store_dir
+#    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#  end
 
+  def cache_dir
+    "#{RAILS_ROOT}/tmp/uploads"
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
