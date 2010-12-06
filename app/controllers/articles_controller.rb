b8adc2779a2c28092168b7a826a1bde4a53c3dfd
Johan Sveholm <jsveholm@gmail.com>
2010-12-07 04:07:04 +0900

    added authentication for the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_filter :authenticate_admin, :except => [:show,:index]
   before_filter :load_article, :only => [:show,:edit,:update,:destroy]
 
   def index
@@ -38,5 +39,15 @@ class ArticlesController < ApplicationController
 
     def load_article
       @article = Article.where(:slug => params[:id]).first
+    end
+
+    def authenticate_admin
+      if !user_signed_in?
+        flash[:alert] = t('devise.failure.unauthenticated')
+        redirect_to new_user_session_path
+      elsif current_user.email != "jsveholm@gmail.com"
+        flash[:alert] = t('You do not have access to that page')
+        redirect_to new_user_session_path
+      end
     end
 end
